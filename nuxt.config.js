87232b081c46413d42897cf6df75129dfbc08cb3
sebastian <sespinozahenriquez@gmail.com>
2022-09-01 22:07:35 -0400

    Añadidos css para botones, texto de cartas

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,11 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    { src: '~assets/style.css', lang: 'css' }
   ],
+    build: {
+      extractCSS: true
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
